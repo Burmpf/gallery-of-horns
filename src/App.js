a519f18d687d2d876b1902d5aca76b19e82847af
Keyan <keyantabor98@gmail.com>
2022-11-08 20:31:28 -0500

    edit to work for netlify

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import Main from './components/Main.js';
-import data from './data.json';
 import '../src/App.css';
 class App extends React.Component {
 render(){
