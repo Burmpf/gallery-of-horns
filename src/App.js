adc918a81bdf4f4c16224c44f00e61f5506bff20
Keyan <keyantabor98@gmail.com>
2022-11-08 20:16:23 -0500

    added data file and bootstrap library

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import Header from './components/Header.js'
-import Footer from './components/Footer.js'
-import Main from './components/Main.js'
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
+import Main from './components/Main.js';
+import data from './data.json';
+import '../src/App.css';
 class App extends React.Component {
 render(){
   return (
